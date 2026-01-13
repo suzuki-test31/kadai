4b3883537032ba31d4d6cd11152ccecf41816fd3
suzuki-test31 <r.suzuki@jqit.co.jp>
2026-01-13 08:46:06 +0000

    fix: clean up workflows and lock file

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
   }
 
   backend "s3" {
-    bucket         = "tfstate-handson-b533f440"  # 出力された値
+    bucket         = "tfstate-handson-b533f440"
     key            = "infrastructure/terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "terraform-lock"
