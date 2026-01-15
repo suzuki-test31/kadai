0c5b2d1999b2ec903824d053b43960b100295125
suzuki-test31 <r.suzuki@jqit.co.jp>
2026-01-15 07:06:49 +0000

    Move phase5-1 structure to root directory

--- assignments/yamada-taro/phase5-1/app/Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM nginx:latest
-COPY index.html /usr/share/nginx/html/index.html
-EXPOSE 80
-CMD ["nginx", "-g", "daemon off;"]
--- assignments/yamada-taro/phase5-1/infrastructure/outputs.tf
+++
@@ -1,7 +0,0 @@
-#================================================
-# Outputs
-#================================================
-output "alb_dns_name" {
-  description = "ALB DNS Name"
-  value       = aws_lb.handson.dns_name
-}
