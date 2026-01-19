34c5476e48414d4005bba929284ec7aa95cf90f4
suzuki-test31 <r.suzuki@jqit.co.jp>
2026-01-19 04:21:36 +0000

    Remove phase5-1 files

--- phase5-1/infrastructure/logs.tf
+++
@@ -1,7 +0,0 @@
-#================================================
-# CloudWatch Log Group for ECS
-#================================================
-resource "aws_cloudwatch_log_group" "ecs_log_group" {
-  name              = "/ecs/handson"
-  retention_in_days = 30
-}
